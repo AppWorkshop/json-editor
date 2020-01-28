da4d3c9fe1d7fa533b725bfd2293e1e36768fe90
Mike Cunneen <mike@appworkshop.net>
2020-01-28 13:19:56 +0800

    fix(*): JSON Editor - Add Lodash templates to EJS
    URLs apparently use the template function to render {{self}}, which EJS broke.
    Slightly hacky fix that checks for "<%" and "{{" before deciding whether to use Lodash or EJS.

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -9066,14 +9066,28 @@ JSONEditor.defaults.templates.ejs = function() {
 
   return {
     compile: function(template) {
-      var compiled = window.EJS.compile(template);
+      // we need to also handle handlebars-like templates e.g.:
+      // template = "blah blah {{self}} blah", context = { self: "value" }
+      // we can use lodash for that.
 
-      return function(context) {
-        context.__now = new Date();
-        context._ = window._;
-        context.moment = window.moment;
-        return compiled(context);
-      };
+      // first, check if the template doesn't have any "<%" string and has a "{{" string
+      if (window._ && template.indexOf("<%") === -1 && template.indexOf("{{") !== -1) {
+        _.templateSettings = {
+          interpolate: /\{\{(.+?)\}\}/g
+        };
+        return _.template(template);
+      } else {
+        // use EJS
+        var compiled = window.EJS.compile(template);
+
+        return function (context) {
+          // console.log("EJS: template: " + template + ", context: " + JSON.stringify(context) );
+          context.__now = new Date();
+          context._ = window._;
+          context.moment = window.moment;
+          return compiled(context);
+        };
+      }
     }
   };
 };
